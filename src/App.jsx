48b6a1a07fea102cd118a74755f17418a91d3cf5
butch888 <never_man@mail.ru>
2025-10-28 01:25:08 +0300

    api get users

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
 const fetchUsers = async () => {
   setLoading(true);
   try {
-    const response = await axios.get('/users');
+    const response = await axios.get('https://express-prisma-versel.vercel.app/users');
     setUsers(response.data);
     
     // Правильное условие для сообщения
